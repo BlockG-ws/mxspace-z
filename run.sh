edec9e71c9e2733c2c30500c560a4602f12c8523
grassblock <hi@imgb.space>
2023-08-14 11:04:54 +0800

    修复数据库相关问题

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-command="node out/index.js --redis_host=redis --db_host=mongo \
+command="node out/index.js --redis_host=${REDIS_HOST} --db_host=${MONGODB_HOST} \
+  --collection_name=mxspace --db_user=${MONGODB_USER} --db_password=${MONGODB_PASSWORD} \
+  --redis_port=${REDIS_PORT} --redis_password=${REDIS_PASSWORD} \
   --allowed_origins=${ALLOWED_ORIGINS} \
   --jwt_secret=${JWT_SECRET} \
   --color \
